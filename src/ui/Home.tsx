1dbbb061557f0c00ab687e2baa874bfb04c2f0c2
Josef8852 <josefaboshady2@gmail.com>
2026-03-02 02:23:37 +0100

    Config Tailwind

--- src/ui/Home.tsx
+++ src/ui/Home.tsx
@@ -1,13 +1,15 @@
-const  Home : React.FC = () => {
+
+
+const Home: React.FC = () => {
   return (
     <div>
-      <h1>
+      <h1 className="text-xl font-semibold text-yellow-500">
         The best pizza.
         <br />
         Straight out of the oven, straight to you.
       </h1>
     </div>
   );
-}
+};
 
 export default Home;
